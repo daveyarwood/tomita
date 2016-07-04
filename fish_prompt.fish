dd83c32e1f3baea1d00a1996cb74e82a6a2939ec
Dave Yarwood <dave.yarwood@gmail.com>
2016-07-03 23:23:26 -0400

    vi mode seems to always be enabled in fish now... ok?

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -7,22 +7,20 @@ function fish_prompt
   set_color normal
   printf '%s ' (__fish_git_prompt)
 
-  if set -q __fish_vi_mode
-    printf '['
-    switch $fish_bind_mode
-      case default
-        set_color --bold red
-        printf 'n'
-      case insert
-        set_color --bold green
-        printf 'i'
-      case visual
-        set_color --bold magenta
-        printf 'v'
-    end
-    set_color normal
-    printf '] '
+  printf '['
+  switch $fish_bind_mode
+    case default
+      set_color --bold red
+      printf 'n'
+    case insert
+      set_color --bold green
+      printf 'i'
+    case visual
+      set_color --bold magenta
+      printf 'v'
   end
+  set_color normal
+  printf '] '
 
   set_color -o yellow
   printf '⋊> '
